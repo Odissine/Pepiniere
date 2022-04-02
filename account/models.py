e80893c2b425c30f93fe19d778ccc70570315b8c
Cyril <albator74@hotmail.com>
2022-04-02 21:36:57 +0200

    Global New Version

--- account/models.py
+++ account/models.py
@@ -8,6 +8,6 @@ class TokenLogin(models.Model):
     user = models.ForeignKey(User, related_name='TokenLogins', null=False, on_delete=models.CASCADE)
 
 
-class Config(models.Model):
-    objects = models.Manager()
-    register = models.BooleanField(default=False)
+# class Config(models.Model):
+#     objects = models.Manager()
+#     register = models.BooleanField(default=False)
